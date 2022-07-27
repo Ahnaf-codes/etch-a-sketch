11fbf3a52339595e5c0190cb96bc455b36d97adb
Ahnaf <ahnafanis0@gmail.com>
2022-07-27 21:41:45 +0600

    added slider text functionality

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
-const sliderValue = document.getElementById("slider").value;
+const slider = document.getElementById("slider");
+
 const sliderText = document.getElementById("sliderText");
 
 function setSliderText() {
+	let sliderValue = document.getElementById("slider").value;
 	sliderText.textContent = `${sliderValue}x${sliderValue}`;
 }
-
 setSliderText();
+slider.addEventListener("input", setSliderText);
