5a9000d067eec6854d72ce9aea98a5a7d20a1221
Ahnaf <ahnafanis0@gmail.com>
2022-07-27 21:44:54 +0600

    strict mode + comments

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
+"use strict";
+// Global Variables
 const slider = document.getElementById("slider");
-
 const sliderText = document.getElementById("sliderText");
 
+//Functions
 function setSliderText() {
 	let sliderValue = document.getElementById("slider").value;
 	sliderText.textContent = `${sliderValue}x${sliderValue}`;
 }
+
+//UI Functionality
 setSliderText();
 slider.addEventListener("input", setSliderText);
