dace7370d09645a3347db19ea6ec8f2b345234ed
Ahnaf <ahnafanis0@gmail.com>
2022-07-30 23:20:17 +0600

    mode selector

--- app.js
+++ app.js
@@ -3,7 +3,9 @@
 const slider = document.getElementById("slider");
 const sliderText = document.getElementById("sliderText");
 const grid = document.getElementById("gridContainer");
+const inks = document.querySelectorAll(".colorBtn");
 const clear = document.getElementById("clearBtn");
+let mode = "pen";
 //Functions
 function setSliderText() {
 	let sliderValue = document.getElementById("slider").value;
@@ -28,8 +30,21 @@ function deleteGrid() {
 		grid.removeChild(grid.lastChild);
 	}
 }
+
+function selectMode() {
+	inks.forEach((ink) => {
+		ink.addEventListener("click", () => {
+			mode = ink.id;
+		});
+	});
+}
+
+
+
 //UI Functionality
 setSliderText();
+createGrid();
 slider.addEventListener("input", setSliderText);
 slider.addEventListener("input", deleteGrid);
 slider.addEventListener("input", createGrid);
+selectMode();
